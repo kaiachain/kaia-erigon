06f35feacf7f7d7c3e54f12f8fc09c5a541742b7
ollie.j <ollie.j@klaytn.foundation>
2025-06-02 16:57:00 +0900

    try 10 nodes

--- erigon-lib/commitment/kaia_test.go
+++ erigon-lib/commitment/kaia_test.go
@@ -88,9 +88,28 @@ func TestKaia_RootHash(t *testing.T) {
 			},
 			common.HexToHash("0x60e8f25e2fb479e625347c1f11e2f07c9cd7d0a5320013294d89281b6fceed4f"),
 		},
+		{
+			accountsTC{
+				{"0xd293f7799bd6c75b82c2802542e2999c14f47e35", "0x01c580648001c0"},
+				{"0xebbbacfb87fbfdec9011cfad2462782b3f8a8d88", "0x01c564808001c0"},
+				{"0xde20ec601c1d770a9ec6e89b2639cd63203514c4", "0x01f849c580808001c0a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a0de75942ed4731d491798b4a82bc5937553e715a97b76fc5987e35ee3cd2ee68380"},
+				// {"0xe8170332ea5a606146e5a9e9deb8dcec0287047c", "0x01c580808001c0"},
+				// {"0xfe90e344274e129d656e03ffd0d0dc0b6518788d", "0x01e680640102a1038318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed75"},
+				// {"0x7bdf109bf068c37c97d5c84196417e034320f3fc", "0x01c781c881c88001c0"},
+				// {"0x69470b1abc45767a122bc5ebb6d8a1c97e4ff935", "0x01f84bc782012c808001c0a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a0d058a2d3336759c22976856de49b828b8282e64b9d2b2d0d0d94fc44928d8e4c80"},
+				// {"0x115cd038daf5cc477c7116beab167e550f41b0d4", "0x01c7820190808001c0"},
+				// {"0x74ef9c1339963462581a5b96720711e7cfec22eb", "0x01f84bc7808201f48001c0a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a067cbae6de159c9d9be7017bb0a3b6be6770c0233c5f4d476523728e0520ca7bb80"},
+				// {"0x5d76c9950b78dcfa47ec195f56d11a4afa8708f3", "0x01f84dc98202588202588001c0a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a02a5787ab5c11885b8b30f1a3aea5c05d48e17eb18f94210555e34a3a965b655a80"},
+			},
+			//common.HexToHash("0x1f359a878bdea26f8eecaa6b6fff8306b870d4b3b18871975aebc68f12c8ea51"),
+			common.HexToHash("0x4a4669650a2fa9da4ac84e32240bf5f780d04a95d277c48f244536c64b8742cc"),
+		},
 	}
 
 	for i, tc := range testcases {
+		if i != 4 {
+			continue
+		}
 		upd := NewUpdates(ModeUpdate, t.TempDir(), KeyToHexNibbleHash)
 		for _, account := range tc.accounts {
 			upd.TouchPlainKey(string(hexutil.MustDecode(account.addressHex)), hexutil.MustDecode(account.accountHex), upd.TouchAccount)
@@ -102,9 +121,11 @@ func TestKaia_RootHash(t *testing.T) {
 		hph.SetTrace(testing.Verbose())
 
 		rootHash, err := hph.Process(ctx, upd, "")
+		spew.Dump(hph.grid)
 		require.NoError(t, err, i)
 		assert.Equal(t, tc.hash.Hex(), common.BytesToHash(rootHash).Hex(), i)
 	}
+	t.Fail()
 }
 
 func TestKaia_RestoreState(t *testing.T) {
